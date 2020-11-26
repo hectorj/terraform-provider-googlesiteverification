6d7125a9667e6461367b7dbb395e5546609ae0d1
hectorj <hector.jusforgues@gmail.com>
2020-11-26 13:31:12 +0100

    Fix #12
    Include Cloudflare provider and DNS record in tests

--- go.mod
+++ go.mod
@@ -3,7 +3,13 @@ module github.com/hectorj/terraform-provider-googlesiteverification
 go 1.14
 
 require (
-	github.com/hashicorp/terraform-plugin-sdk v1.15.0
+	github.com/hashicorp/terraform-plugin-sdk v1.16.0
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	golang.org/x/net v0.0.0-20201031054903-ff519b6c9102 // indirect
 	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	google.golang.org/api v0.29.0
+	github.com/cloudflare/terraform-provider-cloudflare v1.18.2-0.20201126031502-995f63ac2526
 )
+
+replace google.golang.org/genproto v0.0.0-20190819201941-24fa4b261c55 => google.golang.org/genproto v0.0.0-20190927181202-20e1ac93f88c
--- main_test.go
+++ main_test.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"os"
 	"regexp"
 	"testing"
 
+	"github.com/cloudflare/terraform-provider-cloudflare/cloudflare"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
@@ -12,6 +14,7 @@ func TestAccDnsSiteVerification(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		Providers: map[string]terraform.ResourceProvider{
 			"googlesiteverification": Provider(),
+			"cloudflare":             cloudflare.Provider(),
 		},
 		Steps: []resource.TestStep{
 			{
@@ -28,27 +31,51 @@ data "googlesiteverification_dns_token" "example" {
 			},
 			{
 				Config: `
-resource "googlesiteverification_dns" "example" {
+data "googlesiteverification_dns_token" "example" {
 	domain = "test-terraform-provider.hectorj.net"
-	token  = "google-site-verification=8UwI0BG4aaCZxPXXD8J_hvya4LfJLpzS8JjfylQjFtU"
+}
+
+resource "cloudflare_record" "verification" {
+  zone_id = "` + os.Getenv("CLOUDFLARE_ZONE_ID") + `"
+  name    = data.googlesiteverification_dns_token.example.record_name
+  value   = data.googlesiteverification_dns_token.example.record_value
+  type    = data.googlesiteverification_dns_token.example.record_type
+}
+
+resource "googlesiteverification_dns" "example" {
+	domain     = "test-terraform-provider.hectorj.net"
+	token      = data.googlesiteverification_dns_token.example.record_value
+	depends_on = [cloudflare_record.verification]
 }`,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("googlesiteverification_dns.example", "domain", "test-terraform-provider.hectorj.net"),
-					resource.TestCheckResourceAttr("googlesiteverification_dns.example", "token", "google-site-verification=8UwI0BG4aaCZxPXXD8J_hvya4LfJLpzS8JjfylQjFtU"),
+					resource.TestMatchResourceAttr("googlesiteverification_dns.example", "token", regexp.MustCompile(`^google-site-verification=[A-Za-z0-9_]+$`)),
 				),
 			},
 			{
 				Config: `
-resource "googlesiteverification_dns" "example" {
+data "googlesiteverification_dns_token" "example" {
 	domain = "test-terraform-provider.hectorj.net"
-	token  = "google-site-verification=8UwI0BG4aaCZxPXXD8J_hvya4LfJLpzS8JjfylQjFtU"
+}
+
+resource "cloudflare_record" "verification" {
+  zone_id = "` + os.Getenv("CLOUDFLARE_ZONE_ID") + `"
+  name    = data.googlesiteverification_dns_token.example.record_name
+  value   = data.googlesiteverification_dns_token.example.record_value
+  type    = data.googlesiteverification_dns_token.example.record_type
+}
+
+resource "googlesiteverification_dns" "example" {
+	domain     = "test-terraform-provider.hectorj.net"
+	token      = data.googlesiteverification_dns_token.example.record_value
+	depends_on = [cloudflare_record.verification]
 }`,
 				ResourceName:      "googlesiteverification_dns.example",
 				ImportState:       true,
 				ImportStateVerify: true,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("googlesiteverification_dns.example", "domain", "test-terraform-provider.hectorj.net"),
-					resource.TestCheckResourceAttr("googlesiteverification_dns.example", "token", "google-site-verification=8UwI0BG4aaCZxPXXD8J_hvya4LfJLpzS8JjfylQjFtU"),
+					resource.TestMatchResourceAttr("googlesiteverification_dns.example", "token", regexp.MustCompile(`^google-site-verification=[A-Za-z0-9_]+$`)),
 				),
 			},
 		},
