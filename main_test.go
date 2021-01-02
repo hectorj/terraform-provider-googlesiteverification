015303d8b0f6b7894b2ba75a6a3918e00cfedf9f
hectorj <hector.jusforgues@gmail.com>
2021-01-02 11:10:45 +0100

    Fix tests, and remove `depends_on` from the documentation
    The `depends_on` directive prevents the DNS record to be deleted during the retry phase of the verification deletion.

--- main_test.go
+++ main_test.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"regexp"
 	"testing"
 
 	"github.com/cloudflare/terraform-provider-cloudflare/cloudflare"
+	"github.com/google/uuid"
 	"github.com/hashicorp/terraform-plugin-sdk/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/terraform"
 )
 
 func TestAccDnsSiteVerification(t *testing.T) {
+	domain := fmt.Sprintf("%s-test-terraform-provider.hectorj.net", uuid.New())
+
 	resource.Test(t, resource.TestCase{
 		Providers: map[string]terraform.ResourceProvider{
 			"googlesiteverification": Provider(),
@@ -20,19 +24,7 @@ func TestAccDnsSiteVerification(t *testing.T) {
 			{
 				Config: `
 data "googlesiteverification_dns_token" "example" {
-	domain = "test-terraform-provider.hectorj.net"
-}`,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("data.googlesiteverification_dns_token.example", "domain", "test-terraform-provider.hectorj.net"),
-					resource.TestCheckResourceAttr("data.googlesiteverification_dns_token.example", "record_type", "TXT"),
-					resource.TestCheckResourceAttr("data.googlesiteverification_dns_token.example", "record_name", "test-terraform-provider.hectorj.net"),
-					resource.TestMatchResourceAttr("data.googlesiteverification_dns_token.example", "record_value", regexp.MustCompile(`^google-site-verification=[A-Za-z0-9_]+$`)),
-				),
-			},
-			{
-				Config: `
-data "googlesiteverification_dns_token" "example" {
-	domain = "test-terraform-provider.hectorj.net"
+	domain = "` + domain + `"
 }
 
 resource "cloudflare_record" "verification" {
@@ -43,39 +35,12 @@ resource "cloudflare_record" "verification" {
 }
 
 resource "googlesiteverification_dns" "example" {
-	domain     = "test-terraform-provider.hectorj.net"
+	domain     = "` + domain + `"
 	token      = data.googlesiteverification_dns_token.example.record_value
-	depends_on = [cloudflare_record.verification]
 }`,
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("googlesiteverification_dns.example", "domain", "test-terraform-provider.hectorj.net"),
-					resource.TestMatchResourceAttr("googlesiteverification_dns.example", "token", regexp.MustCompile(`^google-site-verification=[A-Za-z0-9_]+$`)),
-				),
-			},
-			{
-				Config: `
-data "googlesiteverification_dns_token" "example" {
-	domain = "test-terraform-provider.hectorj.net"
-}
-
-resource "cloudflare_record" "verification" {
-  zone_id = "` + os.Getenv("CLOUDFLARE_ZONE_ID") + `"
-  name    = data.googlesiteverification_dns_token.example.record_name
-  value   = data.googlesiteverification_dns_token.example.record_value
-  type    = data.googlesiteverification_dns_token.example.record_type
-}
-
-resource "googlesiteverification_dns" "example" {
-	domain     = "test-terraform-provider.hectorj.net"
-	token      = data.googlesiteverification_dns_token.example.record_value
-	depends_on = [cloudflare_record.verification]
-}`,
-				ResourceName:      "googlesiteverification_dns.example",
-				ImportState:       true,
-				ImportStateVerify: true,
-				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("googlesiteverification_dns.example", "domain", "test-terraform-provider.hectorj.net"),
-					resource.TestMatchResourceAttr("googlesiteverification_dns.example", "token", regexp.MustCompile(`^google-site-verification=[A-Za-z0-9_]+$`)),
+					resource.TestCheckResourceAttr("googlesiteverification_dns.example", "domain", domain),
+					resource.TestMatchResourceAttr("googlesiteverification_dns.example", "token", regexp.MustCompile(`^google-site-verification=[A-Za-z0-9_-]+$`)),
 				),
 			},
 		},
